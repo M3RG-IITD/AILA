7330ac583058af909a6cfec97d93e0ab7196bea9
“imandal98” <indrajeetmandal.aaa@gmail.com>
2025-06-19 16:26:46 +0530

    update_5

--- app_old/run_app.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call conda activate afmai
-cd "C:\Users\Admin\Desktop\Automation\AFMBench\Results\app"
-streamlit run app.py
--- app_old/run_qs.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-call conda activate afmai
-cd "C:\Users\Admin\Desktop\Automation\AFMBench\Data"
-streamlit run afm_data_json.py
